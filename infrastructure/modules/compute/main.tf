7bfd086c63ec4c5f8e4ec2bf9d9288c3561dcecd
Dan K. Hansen <dan.kjeldstrom.hansen@proton.me>
2026-07-20 10:23:44 +0200

    feat: Add Longhorn preUpgradeChecker fix and switch to private IP for worker join

--- infrastructure/modules/compute/main.tf
+++ infrastructure/modules/compute/main.tf
@@ -52,7 +52,7 @@ resource "hcloud_server" "worker" {
     k3s_token       = var.k3s_token
     k3s_install_url = "https://get.k3s.io"
     k3s_version     = var.k3s_version
-    master_ip       = hcloud_server.master.network[0].ip
+    master_ip       = [for n in hcloud_server.master.network : n.ip][0]
   })
 }
 
